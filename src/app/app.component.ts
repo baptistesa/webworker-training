08e7568f8ae3833c7b22297d9672cbabb408739d
Baptiste Saint-André <baptiste.saintandre@gmail.com>
2020-11-05 10:27:02 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import isPrimeNumber from 'prime-number';
+import primeNumberList from 'prime-number/list';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webWorkers';
+
+  constructor() {
+    this.runWorker();
+  }
+
+  // Launch the worker
+  runWorker() {
+    if (typeof Worker !== 'undefined') {
+      const worker = new Worker('./app.worker', {
+        type: 'module'
+      });
+      worker.onmessage = ({ data }) => {
+        console.log('Web Worker:', data);
+      };
+      worker.postMessage({});
+    }
+    else {
+
+    }
+  }
+
+  // Launch on the main thread
+  launchMainThread() {
+    const arePrimeList = primeNumberList.map((prime) => {
+      return isPrimeNumber(prime);
+    });
+  }
 }
